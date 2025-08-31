741358a15a62154171a8d635c1a05f44626c464a
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-31 17:39:08 +0530

    day-2

--- chapter-1/first.py
+++ chapter-1/first.py
@@ -1,29 +1,36 @@
 #print("hello world")
 
 #practice question 
-print("""Twinkle, twinkle, little star,
-How I wonder what you are!
-Up above the world so high,
-Like a diamond in the sky.
+#print("""Twinkle, twinkle, little star,
+#How I wonder what you are!
+#Up above the world so high,
+#Like a diamond in the sky.
 
-When the blazing sun is gone,
-When he nothing shines upon,
-Then you show your little light,
-Twinkle, twinkle, all the night.
+#When the blazing sun is gone,
+#When he nothing shines upon,
+#Then you show your little light,
+#Twinkle, twinkle, all the night.
 
-Then the traveler in the dark
-Thanks you for your tiny spark,
-How could he see where to go,
-If you did not twinkle so?
+#Then the traveler in the dark
+#Thanks you for your tiny spark,
+#How could he see where to go,
+#If you did not twinkle so?
 
-In the dark blue sky you keep,
-Often through my curtains peep
-For you never shut your eye,
-Till the sun is in the sky.
+#In the dark blue sky you keep,
+#Often through my curtains peep
+#For you never shut your eye,
+#Till the sun is in the sky.
 
-As your bright and tiny spark
-Lights the traveler in the dark,
-Though I know not what you are,
-Twinkle, twinkle, little star.
-""")
+#As your bright and tiny spark
+#Lights the traveler in the dark,
+#Though I know not what you are,
+#Twinkle, twinkle, little star.
+#""")
+
+#practice question-3
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("my name is sonali jaiswal.i am from gorakhpur uttar pradesh ")
+engine.runAndWait()
+
 
